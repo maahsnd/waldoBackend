a5a58cc04c0ad19a604794b3a36b93c41e4b1387
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-16 16:13:20 -0700

    Add name for reference

--- models/game.js
+++ models/game.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GameSchema = new Schema({
+  name: { type: String, required: true },
   img_link: { type: String, required: true, maxLength: 40 },
   locations: [{ type: Schema.Types.ObjectId, ref: 'Location', required: true }]
 });
