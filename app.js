7d2bac8ef06b6432765120ac15f70f9d8b081e45
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-16 14:09:08 -0700

    Connect to db

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const dotend = require('dotenv').config();
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
+app.use(express.json());
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(process.end.MONGO);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
