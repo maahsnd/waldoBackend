6f6d141761365c2a10ff03f69d3c8fb63ab23160
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-17 12:06:55 -0700

    Fix bug Require location model to address model undefined error. Pass data directly.

--- controllers/game_controller.js
+++ controllers/game_controller.js
@@ -1,6 +1,17 @@
 const asyncHandler = require('express-async-handler');
 const Game = require('../models/game');
+const Location = require('../models/location');
 
 exports.load_game = asyncHandler(async (req, res, next) => {
-  const [imageLink, Locations] = await Game.find({ name: req.body.name });
+  console.log('loading game');
+  try {
+    const game = await Game.find({ name: req.params.name })
+      .populate('locations')
+      .exec();
+    res
+      .status(200)
+      .json({ img_link: game[0].img_link, locations: game[0].locations });
+  } catch (err) {
+    console.log('error' + err);
+  }
 });
