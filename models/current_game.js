889aaa9d4ae23357aea7bcf940c8d84b887ab5a0
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-17 13:18:36 -0700

    Update model Remove all_markers. Already stored in game, thus redundant. Store time in model.

--- models/current_game.js
+++ models/current_game.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CurrentGameSchema = new Schema({
-  found_markers: [{ type: String, required: true }],
-  all_markers: [{ type: String, required: true }]
+  found_markers: [{ type: String }],
+  time: { type: Date, default: Date.now() }
 });
 
 module.exports = mongoose.model('CurrentGame', CurrentGameSchema);
