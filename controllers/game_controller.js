830952e014e07f3de61f2554839f94a2450c5d06
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-17 13:16:37 -0700

    Create new game, pass id on load Refer back to later to get user completion time.

--- controllers/game_controller.js
+++ controllers/game_controller.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const Game = require('../models/game');
+const CurrentGame = require('../models/current_game');
 const Location = require('../models/location');
 
 exports.load_game = asyncHandler(async (req, res, next) => {
@@ -11,8 +12,14 @@ exports.load_game = asyncHandler(async (req, res, next) => {
     const characters = game[0].locations.map((location) => {
       return location.character;
     });
-    res.status(200).json({ img_link: game[0].img_link, characters });
+    const newGame = new CurrentGame({});
+    await newGame.save();
+    res
+      .status(200)
+      .json({ img_link: game[0].img_link, characters, gameId: newGame._id });
   } catch (err) {
     console.log('error' + err);
   }
 });
+
+//start current game timer
