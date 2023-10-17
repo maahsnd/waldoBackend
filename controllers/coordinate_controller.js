74edaad244d7519fa81937d7f3ae25ae9dbc73d9
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-17 14:12:22 -0700

    Send as json, fix bugs Data structures were being referenced incorrectly, causing errors.

--- controllers/coordinate_controller.js
+++ controllers/coordinate_controller.js
@@ -1,51 +1,58 @@
 const asyncHandler = require('express-async-handler');
-const Game = require('../models/game');
+const Location = require('../models/location');
 const CurrentGame = require('../models/current_game');
 
 exports.check_coordinates = asyncHandler(async (req, res, next) => {
-  const locationsArray = await Game.find(
-    { name: req.params.name },
-    'locations'
+  const data = await Location.find(
+    {
+      character: req.body.character
+    },
+    'coords'
   ).exec();
   const click = req.body.coords;
+  const location = data[0].coords;
 
-  const match = locationsArray.find((location) => {
+  const match =
     location.x_min <= click.x &&
-      location.x_max >= click.x &&
-      location.y_min <= click.y &&
-      location.y_max >= click.y;
-  });
+    location.x_max >= click.x &&
+    location.y_min <= click.y &&
+    location.y_max >= click.y;
   //click does not match coordinates
   if (!match) {
-    res.send({
+    res.json({
       found: false,
       win: false
     });
     return;
   }
-  const currentGame = await CurrentGame.findById(req.params.id).exec();
+
+  const currentGame = await CurrentGame.findById(req.body.gameId).exec();
   //click coordinates already found
   if (currentGame.found_markers.includes(match.character)) {
-    res.send({
+    console.error('Already found');
+    res.json({
       found: false,
       win: false
     });
     return;
   }
+
   const foundArr = currentGame.found_markers;
   //click is the last location to be found (win)
-  if (foundArr.length === locationsArray.length - 1) {
-    res.send({
+  if (foundArr.length === currentGame.all_markers.length - 1) {
+    res.json({
       found: true,
       win: true
     });
     return;
   }
+  //else click is found
   const updatedGame = {
     found_markers: [...foundArr, match.character]
   };
+
   await CurrentGame.findByIdAndUpdate(req.params.id, updatedGame, {});
-  res.send({
+  res.json({
     found: true,
     win: false
   });
