48de16dd85a729b03d9b8cfad5e516a90d07df0a
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-16 17:20:49 -0700

    Move coords into location Coords will never be accessed individually.

--- models/location.js
+++ models/location.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 
 const LocationSchema = new Schema({
   character: { type: String, required: true, maxLength: 40 },
-  coords: { type: Schema.Types.ObjectId, ref: 'Coords', required: true }
+  coords: {
+    x: { type: Number, required: true },
+    y: { type: Number, required: true }
+  }
 });
 
 module.exports = mongoose.model('Location', LocationSchema);
