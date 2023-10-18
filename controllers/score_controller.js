e4b8b81a13efd93d2944faaa63bcbf44aadc2554
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-17 17:04:15 -0700

    Create post method Untested.

--- controllers/score_controller.js
+++ controllers/score_controller.js
@@ -14,4 +14,20 @@ exports.get_scores = asyncHandler(async (req, res, next) => {
   }
 });
 
-exports.post_score;
+exports.post_score = asyncHandler(async (req, res, next) => {
+  //start time is Date.now() form, as is endTime
+  const start_time = await CurrentGame.findById(req.body.currentgameId);
+  //get time in seconds from miliseconds
+  const time = (req.body.winTime - start_time) / 1000;
+  const newScore = new Score({
+    name: req.body.username,
+    time,
+    game: req.body.gameId
+  });
+  await newScore.save();
+  const data = await Score.find({ game: req.body.game })
+    .sort('time')
+    .limit(3)
+    .exec();
+  res.json(data);
+});
