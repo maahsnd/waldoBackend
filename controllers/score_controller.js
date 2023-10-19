82ac7d752c5c804f5b829192daa5fca38ac40af4
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-18 18:40:10 -0700

    Create post, update get Use params to get score ref rather than gameId.

--- controllers/score_controller.js
+++ controllers/score_controller.js
@@ -1,10 +1,12 @@
 const asyncHandler = require('express-async-handler');
 const CurrentGame = require('../models/current_game');
 const Score = require('../models/score');
+const Game = require('../models/game');
 
 exports.get_scores = asyncHandler(async (req, res, next) => {
   try {
-    const data = await Score.find({ game: req.body.gameId })
+    const game = await Game.find({ name: req.params.name });
+    const data = await Score.find({ game: game[0]._id })
       .sort('time')
       .limit(3)
       .exec();
@@ -15,19 +17,27 @@ exports.get_scores = asyncHandler(async (req, res, next) => {
 });
 
 exports.post_score = asyncHandler(async (req, res, next) => {
-  //start time is Date.now() form, as is endTime
-  const start_time = await CurrentGame.findById(req.body.currentgameId);
-  //get time in seconds from miliseconds
-  const time = (req.body.winTime - start_time) / 1000;
-  const newScore = new Score({
-    name: req.body.username,
-    time,
-    game: req.body.gameId
-  });
-  await newScore.save();
-  const data = await Score.find({ game: req.body.game })
-    .sort('time')
-    .limit(3)
-    .exec();
-  res.json(data);
+  try {
+    const { username, gameId, currentGameId, winTime } = req.body;
+    // Now you can use these variables in your route handler
+    //start time is Date.now() form, as is endTime
+    const currentGame = await CurrentGame.findById(currentGameId);
+    const start_time = new Date(currentGame.time);
+    //get time in seconds from miliseconds
+    const time = (winTime - start_time.getTime()) / 1000;
+    console.log(start_time.getTime(), winTime, time);
+    const newScore = new Score({
+      name: req.body.username,
+      time,
+      game: req.body.gameId
+    });
+    await newScore.save();
+    const data = await Score.find({ game: req.body.gameId })
+      .sort('time')
+      .limit(3)
+      .exec();
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+  }
 });
