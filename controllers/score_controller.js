726df7567b0d898fc9b21b48210c50afc0ab7215
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-27 14:01:38 -0800

    Remove default name, change query Only return scores with defined name.

--- controllers/score_controller.js
+++ controllers/score_controller.js
@@ -6,7 +6,7 @@ const Game = require('../models/game');
 exports.get_scores = asyncHandler(async (req, res, next) => {
   try {
     const game = await Game.find({ name: 'waldo' });
-    const data = await Score.find({ game: game[0]._id, name: {$ne: 'anonymous'} })
+    const data = await Score.find({ game: game[0]._id, name: {$exists: true} })
       .sort('time')
       .limit(3)
       .exec();
--- models/score.js
+++ models/score.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ScoreSchema = new Schema({
-  name: { type: String, maxLength: 40, default: 'anonymous' },
+  name: { type: String, maxLength: 40 },
   time: { type: Number, required: true },
   game: { type: Schema.Types.ObjectId, ref: 'Game', required: true }
 });
