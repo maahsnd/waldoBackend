392d0b6f6f43c0b965fff6987c4580f4b09104f3
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-27 12:15:31 -0800

    Trust proxy

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
+app.set('trust proxy', 1);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
